ea392c101ff8d3e17ed43c88f43e2af7bd61986b
Justin Hagerty <wheelsmanx@gmail.com>
2021-09-02 12:44:05 -0400

    added webhook.site example

---
+++ SeleniumScripts/example_webhook_site.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+'''
+Generate a end point at webhook and check the request
+going to use https://webhook.site/ 
+1.) go to the site 
+2.) copy link from site and paste it in the double quote of the browser.get() method
+'''
+
+browser = webdriver.Chrome('chromedriver')
+for i in range(1):
+    matched_elements = browser.get("")
